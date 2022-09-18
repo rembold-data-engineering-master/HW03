6ab85b136b53cf4e527c1270982b0ecf890fab46
Jed Rembold <jedediah.r@gmail.com>
2022-09-18 11:14:57 -0700

    Update with templates and new due date

---
+++ Problem1.sql
@@ -0,0 +1,56 @@
+/*Problem 2
+  Who did you work with (if anyone)?:
+  Estimated time spent on problem?:
+
+As usual, you have space to fill in both your answer and the accompanying SQL statement. If an answer necessitated more that just reading a number off your table, please provide a short description below the answer of how you found your solution.
+
+*/
+
+-------- PART A --------
+-- Answers:
+-- SQL below
+
+
+
+-------- PART B --------
+-- Answer:
+-- SQL below
+
+
+
+-------- PART C --------
+-- Answers:
+-- SQL below
+
+
+
+-------- PART D --------
+-- Answer:
+-- SQL below
+
+
+
+-------- PART E --------
+-- Answer:
+-- SQL below
+
+
+
+-------- PART F --------
+-- Answer:
+-- SQL below
+
+
+
+-------- PART G --------
+-- Answers:
+-- SQL below
+
+
+
+-------- PART H --------
+-- Answers:
+-- SQL below
+
+
+
---
+++ Problem2.sql
@@ -0,0 +1,10 @@
+/*Problem 3
+  Who did you work with (if anyone)?:
+  Estimated time spent on problem?:
+
+This problem almost assuredly will take you multiple steps. Include your used SQL queries, and provide some text explanation of what you were doing in between using comments! **Don't forget to upload your output CSV as well as this file.**
+*/
+
+-- Alternate SQL and explanatory comments below as you work out the problem
+
+
